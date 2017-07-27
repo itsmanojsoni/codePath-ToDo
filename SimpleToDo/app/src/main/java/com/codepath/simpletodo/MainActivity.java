60d1d718ac292daa35babdda29b071ce6c5eaadc
manoj <manoj@flipagram.com>
2017-07-26 17:20:58 -0700

    added recycle View

--- SimpleToDo/app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ SimpleToDo/app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -1,13 +1,42 @@
 package com.codepath.simpletodo;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.codepath.simpletodo.dummy.DummyContent;
+
+
+public class MainActivity extends AppCompatActivity  implements ItemFragment.OnListFragmentInteractionListener{
+
+    private final String TODO_LIST_FRAGMENT_TAG = "TODO_LIST_FRAGMENT_TAG";
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.todolist);
+
+
+        ItemFragment listFragment =
+                (ItemFragment) getSupportFragmentManager().findFragmentByTag(TODO_LIST_FRAGMENT_TAG);
+
+        if (listFragment == null) {
+            Log.d("MainActivity", "gListFragment is null");
+            listFragment = ItemFragment.newInstance(1);
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.listContainer, listFragment, TODO_LIST_FRAGMENT_TAG);
+            fragmentTransaction.commit();
+        } else {
+
+            Log.d("MainActivity", "list Fragment not null");
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+        Log.d(TAG, "Item Clicked");
     }
 }
